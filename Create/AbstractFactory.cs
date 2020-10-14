82ebd8d771fd9e6b8be56ecdaa278beab36d38a6
ChunibyouH <ycyzxz@gmail.com>
2020-10-14 18:12:33 +0800

    抽象工厂

---
+++ Create/AbstractFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Create
+{
+    /// <summary>
+    /// 抽象工厂
+    /// 设计原则：遵循单一职责、依赖倒置、开闭原则
+    /// </summary>
+    public class AbstractFactory
+    {
+    }
+}
